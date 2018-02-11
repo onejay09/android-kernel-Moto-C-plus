841ef208d8d0e08007466efaffd2db8531126417
DhirajMS137 <dms25121997@gmail.com>
2018-02-11 12:56:13 +0530

    Already defined user and host

--- build.sh
+++ build.sh
@@ -14,8 +14,8 @@ git clone https://github.com/DhirajSurvase/GCC_4.8 toolchain
 export ARCH=arm
 
 #host and user
-export KBUILD_BUILD_USER="DhirajMS"
-export KBUILD_BUILD_HOST="Dracarys"
+#export KBUILD_BUILD_USER="DhirajMS"
+#export KBUILD_BUILD_HOST="Dracarys"
 
 #Defconfig for Moto C plus
 #make -C $PWD O=$PWD/out ARCH=arm A158_defconfig
