9d0b12938fe51f52058ca34773e952561824261b
DhirajSurvase <dms25121997@gmail.com>
2018-03-01 00:04:03 +0000

    update lcm

--- build.sh
+++ build.sh
@@ -24,12 +24,12 @@ export ARCH=arm
 
 #get toolchain gcc-4.9
 git clone https://github.com/DhirajSurvase/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 gcc4.9
-export CROSS_COMPILE=/home/ubuntu/kcp/gcc4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/home/ubuntu/los/kernel/moto/panelli/gcc4.9/bin/arm-linux-androideabi-
 
 
 #Defconfig for Moto C plus
 #make -C $PWD O=$PWD/out ARCH=arm A158_defconfig
-make -C $PWD O=$PWD/out ARCH=arm A158_debug_defconfig
+make -C $PWD O=$PWD/out ARCH=arm panelli_defconfig
 
 
 #CompileNow
