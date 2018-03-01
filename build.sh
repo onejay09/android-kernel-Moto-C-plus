bc2cfabce9ee329354003180ab6e61164141c65e
DhirajSurvase <dms25121997@gmail.com>
2018-03-01 00:04:03 +0000

    add lcm hct_nt35521s_dsi_vdo_hd_boe_50_xld

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ export ARCH=arm
 
 #get toolchain gcc-4.8
 git clone https://github.com/DhirajSurvase/GCC_4.8 gcc4.8
-export CROSS_COMPILE=/home/ubuntu/los/kernel/moto/panelli/gcc4.8/bin/arm-eabi-
+export CROSS_COMPILE=/home/younilban/kernel/panelli/gcc4.8/bin/arm-eabi-
 
 #get toolchain gcc-4.9
 #git clone https://github.com/DhirajSurvase/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 gcc4.9
@@ -29,7 +29,7 @@ export CROSS_COMPILE=/home/ubuntu/los/kernel/moto/panelli/gcc4.8/bin/arm-eabi-
 
 #Defconfig for Moto C plus
 #make -C $PWD O=$PWD/out ARCH=arm A158_defconfig
-make -C $PWD O=$PWD/out ARCH=arm A158_debug_defconfig
+make -C $PWD O=$PWD/out ARCH=arm panelli_defconfig
 
 
 #CompileNow
